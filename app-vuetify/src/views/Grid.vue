e90f55c8c205a8be083817155f0ebd9467bd4990
francisco ramirez <gforce3002@gmail.com>
2021-10-15 14:07:24 -0500

    Creacion de modulo vuetify

---
+++ app-vuetify/src/views/Grid.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12" sm="6">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia a iste modi, aliquid doloribus odio qui praesentium quam fugit omnis maiores deserunt similique aperiam et animi, alias, quos consectetur accusantium!</p>
+            </v-col>
+             <v-col cols="12" sm="6">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia a iste modi, aliquid doloribus odio qui praesentium quam fugit omnis maiores deserunt similique aperiam et animi, alias, quos consectetur accusantium!</p>
+            </v-col>
+        </v-row>
+    </v-container>
+  
+</template>
+
+<script>
+export default {
+    name:"Grid"
+}
+</script>
+
---
+++ app-vuetify/src/views/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>hola soy un nav</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
