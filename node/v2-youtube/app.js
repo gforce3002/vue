edf06fdf1a4b7868e691f46ae507649b1eda27e3
francisco ramirez <gforce3002@gmail.com>
2021-10-21 15:59:24 -0500

    mevn configurando middlewares

--- node/v2-youtube/app.js
+++ node/v2-youtube/app.js
@@ -1,11 +1,41 @@
-const express = require('express');
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors'
+import path from 'path';
+
 const app = express();
 const port= 8080;
 
-app.get('/', function(req,res){
-    res.send("Hola mundo")
-})
+//MIDDLEWARE
+/**Morgan se utiliza para visualizar las peticiones que hagan el usuario 
+ * atravez de los verbos http como es GET, PUT, DELETE, POST
+ */
+app.use(morgan('tiny'))
 
-app.listen(port,()=>{
-    console.log(`El servidor esta escuchando en http://localhost:${port}`)
+/**Cors permite para validar peticiones de otros dominios */
+app.use(cors())
+
+/**convertir peticiones en json */
+app.use(express.json())
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({extended:true}))
+
+//RUTAS
+
+/* app.get('/', function(req,res){
+    res.send("Hola mundoonno")
+}) */
+
+
+//Middleware para Vue.js router modo history
+/**Esta linea sirve para visualizar todas las paginas o las vistas ui del sitio web
+ * de acuerdo al directorio que se determine
+ */
+ const history = require('connect-history-api-fallback')
+ app.use(history())
+ app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('puerto', process.env.PORT || port)
+app.listen(app.get('puerto'),()=>{
+    console.log(`El servidor esta escuchando en http://localhost:${app.get('puerto')}`)
 })
